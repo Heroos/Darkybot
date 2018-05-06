7ba515d8da624efd2c1187f273ec658cc670e390
Darkyyyyy <30601244+Darkyyyyy@users.noreply.github.com>
2018-05-06 22:49:15 +0200

    Index.js mis a jour ! (06/05/18)

--- index.js
+++ index.js
@@ -214,8 +214,8 @@ let botembed = new Discord.RichEmbed()
 	.setThumbnail("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Emoji_u1f4dd.svg/1000px-Emoji_u1f4dd.svg.png")
 	.addField("kick <membre> <raison>","Pour l'exclure du serveur !")
   .addField("ban <membre> <raison>", "Pour le frapper avec le marteau du ban ! èwé")
-  .addField("giverole <membre> <role>", "Pour donner a un membre le rôle choisi. **[NON FONCTIONELLE]**")
-  .addField("removerole <membre> <role>", "Pour retirer a un membre le rôle choisi. **[NON FONCTIONELLE]**")
+  .addField("giverole <membre> <role>", "Pour donner a un membre le rôle choisi.")
+  .addField("removerole <membre> <role>", "Pour retirer a un membre le rôle choisi.")
 
 return message.channel.send(botembed);
 }
@@ -226,13 +226,13 @@ if (message.content.startsWith(prefix + "hug")){
 let toHug = message.mentions.users.first() || client.users.get(args[0]);
  if (!toHug) return message.reply("Alors, euh... Je ne sait pas si caliner l'air est la meilleur chose.");
  if (toHug.id == message.author.id) return message.reply("Te faire un calin toi même ? Pourquoi pas, c'est toi qui voit.");
- if (toHug.id == client.user.id) return message.reply("Me faire a calin a moi et comme faire un calin a quelqu'un qui n'éxiste pas, enfaite...");
+ if (toHug.id == client.user.id) return message.reply("me faire a calin a moi et comme faire un calin a quelqu'un qui n'éxiste pas, enfaite...");
 var replies = ["https://media1.tenor.com/images/b77fd0cfd95f89f967be0a5ebb3b6c6a/tenor.gif?itemid=7864716", "https://media1.tenor.com/images/b87f8b1e2732c534a00937ffb24baa79/tenor.gif?itemid=9136391", "https://media1.tenor.com/images/40aed63f5bc795ed7a980d0ad5c387f2/tenor.gif?itemid=11098589", "https://media1.tenor.com/images/a2b621c6c769eee24e03b97990c15699/tenor.gif?itemid=4631839", "https://media1.tenor.com/images/bb841fad2c0e549c38d8ae15f4ef1209/tenor.gif?itemid=10307432", "https://media1.tenor.com/images/b0de026a12e20137a654b5e2e65e2aed/tenor.gif?itemid=7552093"]
 var result = Math.floor((Math.random() * replies.length));
 
  let botembed = new Discord.RichEmbed()
- .setDescription(`**${toHug.username}** tu reçois un gros calin de la part de **${message.author.username}** ! :wink: `)
- .setColor("#FF8CFB")
+ .setDescription(`**${toHug.username}**, tu reçois un gros calin de la part de **${message.author.username}** ! :wink: `)
+ .setColor("#ff30ce")
  .setImage(replies[result]);
 
 
@@ -262,7 +262,7 @@ if (message.content.startsWith(prefix + "giverole")){
     let gRole = message.guild.roles.find("name", role);
     if(!gRole) return message.reply("Je n'ai pas trouver le rôle.");
 
-   if(!rMember.roles.has(gRole.id))
+   if(!rMember.roles.has(gRole.id)) return message.reply("Il possède déja ce rôle.")
      (rMember.addRole(gRole.id));
 
    try{
